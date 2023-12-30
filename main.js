c3a7b1cfa703a7068b40a0c7bdf89912042ad8a8
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-12-30 11:47:34 +0200

    ExempluNodeJS

---
+++ main.js
@@ -0,0 +1,25 @@
+// Exemplu de cod pentru adăugarea unui proiect
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let proiecte = []; // Array pentru stocarea proiectelor
+
+app.post('/adauga-proiect', (req, res) => {
+  const { numeProiect, livrabilePartiale } = req.body;
+
+  const proiect = {
+    numeProiect,
+    livrabilePartiale,
+    evaluatori: [], // Lista cu evaluatori
+  };
+
+  proiecte.push(proiect);
+  res.status(201).json({ succes: true, proiect });
+});
+
+app.listen(3000, () => {
+  console.log('Serverul rulează pe portul 3000');
+});
