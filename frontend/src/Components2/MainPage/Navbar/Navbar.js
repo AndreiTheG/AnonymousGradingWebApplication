592e27be8d38a9a09e61e6f2d4a928d25a19669e
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2024-01-22 16:37:08 +0200

    O linie de cod corectata

--- frontend/src/Components2/MainPage/Navbar/Navbar.js
+++ frontend/src/Components2/MainPage/Navbar/Navbar.js
@@ -19,7 +19,6 @@ const navbar = (props) => {
             <img rel="icon" style={{maxHeight: "50px"}} src={logo} alt="Logo"></img>
             </div>
             <div style={{float: "left"}}>
-                {/* <Link to="/home/profile/project" style={{ marginLeft: "40px" }}>Profile</Link> */}
                 <Link to="/home/project-list" style={{ marginLeft: "40px" }}>Projects</Link>
             </div>
 
